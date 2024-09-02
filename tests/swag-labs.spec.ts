289a8824dec640e5cdad362184c00d6170385697
Heath Hughes <heath.hughes@daveramsey.com>
2024-09-02 16:01:39 -0500

    removed old comments

--- tests/swag-labs.spec.ts
+++ tests/swag-labs.spec.ts
@@ -3,10 +3,6 @@ import { test, expect } from '@playwright/test';
 test('can login', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/v1/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Swag Labs/);
-
-  // Fill an input with the id "username"
   await page.locator('data-test=username').fill('standard_user');
   await page.locator('data-test=password').fill('secret_sauce');
   await page.locator('id=login-button').click();
