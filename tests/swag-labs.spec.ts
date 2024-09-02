476c3be18298d0261416c76f12ce1c608c0a6e8a
Heath Hughes <heath.hughes@daveramsey.com>
2024-09-02 16:00:18 -0500

    added login test

--- tests/swag-labs.spec.ts
+++ tests/swag-labs.spec.ts
@@ -1,18 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('can login', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/v1/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Swag Labs/);
+
+  // Fill an input with the id "username"
+  await page.locator('data-test=username').fill('standard_user');
+  await page.locator('data-test=password').fill('secret_sauce');
+  await page.locator('id=login-button').click();
+  await page.locator('id=shopping_cart_container').click();
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
