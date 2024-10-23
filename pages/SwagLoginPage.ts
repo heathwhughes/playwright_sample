3f2341c3c810ca31d7f436268892093bd3ad9605
Heath Hughes <heath.hughes@daveramsey.com>
2024-10-23 16:40:41 -0500

    added wikipedia test

--- pages/SwagLoginPage.ts
+++
@@ -1,7 +0,0 @@
-export class SwagLoginPage {
-    static readonly LOGIN_PAGE_URL = 'https://www.saucedemo.com/v1/';
-    static readonly USERNAME_INPUT = 'data-test=username';
-    static readonly PASSWORD_INPUT = 'data-test=password';
-    static readonly LOGIN_BUTTON = 'id=login-button';
-  }
-  
