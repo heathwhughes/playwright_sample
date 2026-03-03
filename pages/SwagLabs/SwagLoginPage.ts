54354200ce471131a69aa528b99b4376ea87fb52
Heath Hughes <heathwhughes@gmail.com>
2026-03-02 20:57:27 -0600

    fixed test and refactored

--- pages/SwagLabs/SwagLoginPage.ts
+++ pages/SwagLabs/SwagLoginPage.ts
@@ -1,7 +1,15 @@
-export class SwagLoginPage {
-    static readonly LOGIN_PAGE_URL = 'https://www.saucedemo.com/v1/';
-    static readonly USERNAME_INPUT = 'data-test=username';
-    static readonly PASSWORD_INPUT = 'data-test=password';
-    static readonly LOGIN_BUTTON = 'id=login-button';
+import { BasePage } from '../BasePage';
+
+export class SwagLoginPage extends BasePage {
+  readonly loginPageUrl = 'https://www.saucedemo.com/v1/';
+  readonly usernameInput = 'data-test=username';
+  readonly passwordInput = 'data-test=password';
+  readonly loginButton = 'id=login-button';
+
+  async login(username: string, password: string) {
+    await this.page.goto(this.loginPageUrl);
+    await this.page.locator(this.usernameInput).fill(username);
+    await this.page.locator(this.passwordInput).fill(password);
+    await this.page.locator(this.loginButton).click();
   }
-  
+}
